3eef579104b5f8552f83c114e6e11bec97c9af5e
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2025-02-17 12:23:39 +0530

    kth largest element.java

---
+++ POTD/kth largest element.java
@@ -0,0 +1,6 @@
+Arrays.sort(arr);
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
